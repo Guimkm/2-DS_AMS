f0a974bc6cda112b46868dffed294f9b85063760
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2026-03-12 13:40:53 -0300

    Delete Aula 11-03-2026 directory

--- Aula 11-03-2026/Exercícios/calculo_de_temperatura/Kelvin.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.calculo_de_temperatura;
-
-public class Kelvin {
-    public static double Kelvin(double t) {
-         double res  = t - 273.15;
-        return res;
-    }
-}
