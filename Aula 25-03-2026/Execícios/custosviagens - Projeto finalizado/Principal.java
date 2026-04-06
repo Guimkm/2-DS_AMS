d797812904c05922c9518bceae89c4a5a605ead0
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2026-04-05 21:44:33 -0300

    Add files via upload

---
+++ Aula 25-03-2026/Execícios/custosviagens - Projeto finalizado/Principal.java
@@ -0,0 +1,9 @@
+package com.mycompany.custosviagens;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        MenuCustosViagem menu = new MenuCustosViagem();
+        menu.executarCustosViagem();
+    }
+}
