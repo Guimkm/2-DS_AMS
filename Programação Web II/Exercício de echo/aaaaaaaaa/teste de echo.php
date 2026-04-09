045329ad650cd21beb03f8293e0adbc4d41f90c9
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2026-04-08 21:05:56 -0300

    Add files via upload

---
+++ Programação Web II/Exercício de echo/aaaaaaaaa/teste de echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Echo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <?php
+        $idade = 17;
+        $dataNasc = "04/04/2008";
+        $nome = "Guilherme";
+        $sobrenome = "Araujo Carneiro da Silva";
+
+        echo "Meu nome é $nome $sobrenome<br>";
+        echo "nasci no dia $dataNasc<br>";
+        echo "e tenho $idade anos<br>";
+      ?>
+    </div>
+</body>
+</html>
