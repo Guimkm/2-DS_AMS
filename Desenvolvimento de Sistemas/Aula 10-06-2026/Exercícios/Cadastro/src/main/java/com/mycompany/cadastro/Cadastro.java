892c0c2d0725e137a5f24cc55c6bf3f2f463dc74
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2026-06-13 20:29:05 -0300

    Add files via upload

---
+++ Desenvolvimento de Sistemas/Aula 10-06-2026/Exercícios/Cadastro/src/main/java/com/mycompany/cadastro/Cadastro.java
@@ -0,0 +1,13 @@
+package com.mycompany.cadastro;
+
+/**
+ *
+ * @author guilherme
+ */
+public class Cadastro {
+
+    public static void main(String[] args) {
+        Interface app = new Interface();
+        app.main(args);
+    }
+}
